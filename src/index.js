7ded29848dd2a328b7ec710bc5174c88baaede6e
Kandy Peter <kandypeter03@gmail.com>
2022-01-11 13:39:27 +0200

    install webpack packages and add files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from '../node_modules/lodash/lodash.js';
+import {hello} from './hello';
+
+hello();
+hello();
+hello();
+hello();
+console.log('Hello webpack');
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
